0fc5d1fe733f5d9354c397755bead3914672a328
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-05-09 13:38:25 +1000

    feat: add canonical tag to fe

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -4,12 +4,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import "../styles/tailwind.css";
 import NextProgress from "next-progress";
+import { useRouter } from "next/router.js";
+import Head from "next/head.js";
 
 export default function App({ Component, pageProps }: any) {
     const [queryClient] = useState(() => new QueryClient());
+    const router = useRouter();
+    const canonicalUrl = (
+        `https://usemiller.dev` + (router.asPath === "/" ? "" : router.asPath)
+    ).split("?")[0];
     return (
         <QueryClientProvider client={queryClient}>
             <UserProvider>
+                <Head>
+                    <link rel="canonical" href={canonicalUrl} />
+                </Head>
                 <NextProgress delay={300} options={{ showSpinner: true }} />
                 <Component {...pageProps} />
             </UserProvider>
