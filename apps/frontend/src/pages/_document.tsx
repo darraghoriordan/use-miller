c2b4075a4e9230d75151e998552e3a71bc25598d
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-09-18 22:52:25 +1000

    feat: remove plausible, add chat

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import * as React from "react";
+import Script from "next/script";
 
 export default function Document(props: any) {
     return (
@@ -18,12 +19,14 @@ export default function Document(props: any) {
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
                 />
-                <script
-                    defer
-                    data-domain="usemiller.dev"
-                    data-api="/plaus/api/event"
-                    src="/plaus/js/script.js"
-                ></script>
+                <Script type="text/javascript">
+                    {`window.$crisp=[];
+    window.CRISP_WEBSITE_ID="0c58d997-a702-4b2d-86be-e84db144747c";
+    (function(){d=document;s=d.createElement("script");
+    s.src="https://client.crisp.chat/l.js";
+    s.async=1;d.getElementsByTagName("head")[0].appendChild(s);
+    })();`}
+                </Script>{" "}
             </Head>
             <body>
                 <Main />
