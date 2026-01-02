0b3a74a2d2420caddb4374c39bdbd0ce6789bf5d
Darragh ORiordan <darragh.oriordan@gmail.com>
2026-01-02 23:12:36 +1000

    fix: minor fixes

--- apps/backend/src/course-files/UserDiscriminatedCacheInterceptor.ts
+++ apps/backend/src/course-files/UserDiscriminatedCacheInterceptor.ts
@@ -1,4 +1,4 @@
-import { RequestWithUser } from "@darraghor/nest-backend-libs";
+import { RequestUser, RequestWithUser } from "@darraghor/nest-backend-libs";
 import { ExecutionContext, Injectable, Logger } from "@nestjs/common";
 import { CacheInterceptor } from "@nestjs/cache-manager";
 
@@ -13,7 +13,9 @@ export default class UserDiscriminatedCacheInterceptor extends CacheInterceptor 
             .switchToHttp()
             .getRequest<RequestWithUser>();
 
-        const userUuid = httpContext.user.uuid || "anonymous";
+        // User may be undefined at runtime in an interceptor before auth middleware populates it
+        const user = httpContext.user as RequestUser | undefined;
+        const userUuid = user?.uuid ?? "anonymous";
         const cacheKey = `${userUuid}${httpContext.method}${httpContext.url}`;
         this.logger.debug({ cacheKey }, "Using cache key");
         return cacheKey;
