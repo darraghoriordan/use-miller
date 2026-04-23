685c1f6dd601e243d68f1a988a0ee7275f39059f
Darragh ORiordan <darragh.oriordan@gmail.com>
2026-04-23 15:21:34 +1000

    feat: fix migration naming

--- apps/backend/src/main.module.ts
+++ apps/backend/src/main.module.ts
@@ -1,28 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import {
-    CoreModule,
-    AuthzModule,
-    AuthzClientModule,
-    OrganisationModule,
-    UserInternalModule,
-    SmtpEmailClientModule,
-    SuperPowersModule,
-    InvitationModule,
-    DatabaseModule,
-} from "@darraghor/nest-backend-libs";
-import { UserOnboardingModule } from "./user-onboarding/user-onboarding.module.js";
-import { PaymentsModule } from "./payments/payments.module.js";
-import { CourseFilesModule } from "./course-files/course-files.module.js";
-import { SubscriptionAssetsModule } from "./subscription-assets/sub-assets.module.js";
-import { OpenTelemetryModule } from "nestjs-otel";
+import { CoreModule } from "@darraghor/nest-backend-libs";
 import {
     createCoreConfig,
     createLoggerConfig,
-    createAuthzConfig,
-    createAuthzClientConfig,
-    createSmtpEmailConfig,
-    createInvitationConfig,
 } from "./config/library.config.js";
 
 @Module({
@@ -41,49 +22,6 @@ import {
                 useFactory: createLoggerConfig,
             },
         }),
-
-        OpenTelemetryModule.forRoot({
-            metrics: {
-                hostMetrics: true,
-            },
-        }),
-
-        DatabaseModule,
-
-        AuthzModule.forRootAsync({
-            imports: [
-                ConfigModule,
-                AuthzClientModule.forRootAsync({
-                    imports: [ConfigModule],
-                    inject: [ConfigService],
-                    useFactory: createAuthzClientConfig,
-                }),
-            ],
-            inject: [ConfigService],
-            useFactory: createAuthzConfig,
-        }),
-
-        UserInternalModule,
-
-        SmtpEmailClientModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: createSmtpEmailConfig,
-        }),
-
-        OrganisationModule,
-        UserOnboardingModule,
-        PaymentsModule,
-        CourseFilesModule,
-        SuperPowersModule,
-
-        InvitationModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: createInvitationConfig,
-        }),
-
-        SubscriptionAssetsModule,
     ],
     controllers: [],
     providers: [],
